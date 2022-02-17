439204ee4600648fbc9a2f515cf9669e2ccdf068
DaHbka <flexinzflexinz@gmail.com>
2022-02-17 21:50:07 +0300

    16 has been completed

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+class First {
+    hello() {
+        console.log('Привет я метод родителя!');
+    }
+}
 
+class Second extends First {
+    hello() {
+        super.hello();
+        console.log('А я наследуемый метод!');
+    }
+}
